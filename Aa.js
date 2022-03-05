ea44b63ea9befbb42868458997fcae87e3f3b7e3
JanRizzi <janrizzi71@gmail.com>
2022-03-05 17:55:33 -0300

    primeira insercao no Git

---
+++ Aa.js
@@ -0,0 +1,17 @@
+function imprime(texto) {
+    if (texto == "foda-se")
+       console.log(texto + " sou mal educado");
+
+       else
+       console.log(`${texto} sou bem legal`);
+}
+  
+
+let texto ="oi";
+imprime(texto);
+
+texto = "foda-se";
+imprime(texto);
+
+texto = "meu caralho"
+imprime(texto);
---
+++ teste.js
@@ -0,0 +1,7 @@
+let telefone = 12341234;
+console.log("O telefone é " + String(telefone)); 
+// teremos a conversão do número 12341234 para uma string “12341234” e assim poderemos fazer a concatenação entre as strings
+let usuarioConectado = false;
+console.log(String(usuarioConectado)); // teremos a conversão da booleana para string, nesse caso teremos uma string “false”.
+usuarioConectado = true;
+console.log(String(usuarioConectado)); // agora teremos uma string “true”.
