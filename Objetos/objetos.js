d1a85bfd7aa7f4b387c8a211a26d0521aec95134
JanRizzi <janrizzi71@gmail.com>
2022-04-11 15:04:42 -0300

    continuando com objetos

--- Objetos/objetos.js
+++ Objetos/objetos.js
@@ -10,7 +10,7 @@ const cliente = {
     cpf: "15415415477",
     email: "andre@email.com.br"
 }
-
+console.log('--------------------------------')
 // Desafio 1 print infos
 
 console.log(`meu nome é ${cliente.nome} e tenho ${cliente.idade} anos`)
@@ -18,13 +18,14 @@ console.log(`meu nome é ${cliente.nome} e tenho ${cliente.idade} anos`)
 //substring exibe parte de uma string
 console.log(cliente.cpf.substring(0, 3))
 
+console.log('--------------------------------')
 // desafio 2 acessar lista
 // ao invés de usar notação de ponto se usa colchetes
 
 const chaves = ['nome', 'idade', 'cpf', 'email']
 
 console.log(cliente[chaves[0]])
-console.log('--------------------------------')
+
 chaves.forEach(info => {
     if (info == 'cpf') {
         console.log(`${cliente.cpf.substring(0, 3)}********`)
@@ -32,16 +33,53 @@ chaves.forEach(info => {
         console.log(cliente[info])
     }
 })
-console.log('--------------------------------')
 
 console.log(cliente['email'])
 
+console.log('--------------------------------')
 // desafio 3 adicionar campos
 
-cliente.fone="991382857"
+cliente.fone = "991382857"
 console.log(cliente)
 
-delete cliente.idade
+delete cliente.fone
 console.log(cliente)
 
+console.log('--------------------------------')
+//desafio 4 imprimir telefones
+
+cliente.fones = ["47991382857", "47991463556"]
+cliente.fones.forEach(fone => console.log(fone))
+
+console.log('--------------------------------')
+//desafio 5 objeto dentro de objeto
+
+cliente.dependente = {
+    nome: 'sara',
+    idade: 16,
+    parentesco: 'filha'
+}
+
+//alterando
+
+cliente.dependente.nome = 'Sara winter'
+console.log(cliente)
+
+console.log('--------------------------------')
+// desafio 6 criando dependentes como arrays
+
+delete cliente.dependente
+console.log(cliente)
+
+cliente.dependentes = [{
+    nome: 'Sara Winter',
+    idade: 19,
+    parentesco: 'filha'
+}, {
+    nome: 'Sandra Bigorna',
+    idade: 13,
+    parentesco: 'filha'
+}]
+
+console.log(cliente)
 
